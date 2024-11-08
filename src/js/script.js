5002d6324a034502e7c934ac9739e18610f22865
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-11-08 10:55:33 +0100

    corection du script js

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,13 @@
 // TODO
-const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
-aMettreEnRouge.style.color="red"
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color="red";
 
-const enROugeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 enRougeSuiteAClick.addEventListener("click", (evt)=>{
-  enROugeSuiteAClick.style.color="red"
+  enRougeSuiteAClick.style.color="red"
 });
 
-const collH2 = document.querySelectorAll("h2")
-NodeList(3) [h2, h2, h2]
+const collH2 = document.querySelectorAll("h2");
 collH2.forEach((elm)=>{
     elm.addEventListener("click",(evt)=>{
         evt.target.style.color="red";
