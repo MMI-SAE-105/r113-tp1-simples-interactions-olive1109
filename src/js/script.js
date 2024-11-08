5e82627739f01f4f01e0ab9878b1340554f9dab2
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-11-08 10:40:25 +0100

    écriture des scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+aMettreEnRouge.style.color="red"
+
+const enROugeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+enRougeSuiteAClick.addEventListener("click", (evt)=>{
+  enROugeSuiteAClick.style.color="red"
+});
+
+const collH2 = document.querySelectorAll("h2")
+NodeList(3) [h2, h2, h2]
+collH2.forEach((elm)=>{
+    elm.addEventListener("click",(evt)=>{
+        evt.target.style.color="red";
+    });
+});
